921f8689956fd7ee0e9646f11e4b01822306a3b5
Mehdi Fekih <sixfoursuited@gmail.com>
2024-07-19 23:46:02 +0200

    fetching of all static data works fine now

--- data/api_calls/api_payload.py
+++ data/api_calls/api_payload.py
@@ -1,23 +1,34 @@
 import requests
+import random
 
 def get_access_token():
-    token_url = "https://api.lufthansa.com/v1/oauth/token"
-    payload = {
+    url_token = "https://api.lufthansa.com/v1/oauth/token"
+    payload = list()
+    payload.append({
         'client_id': "vss6aqb3gf2czn5syafmbrzp3",
         'client_secret': "VzZCePAxmQ",
         'grant_type': 'client_credentials'
-    }
-    headers = {
+    })
+    payload.append({
+        'client_id': "v47w6dvsccvpx8j46ymvbtzkn",
+        'client_secret': "yqgD6EzQmb",
+        'grant_type': 'client_credentials'
+    })
+    payload.append({
+        'client_id': "m7eccvja28j882ndzn4h46fuh",
+        'client_secret': "nu228RpY9M",
+        'grant_type': 'client_credentials'
+    })
+    headers_token = {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
-    response = requests.post(token_url, data=payload, headers=headers)
-    if response.status_code == 200:
-        token_info = response.json()
-        return token_info['access_token']
-    else:
-        print(f"Error: {response.status_code}")
-        print(response.json())
-        return None
+    
+    pay = payload[random.randint(0,len(payload)-1)]
+    print(pay)
+    response_token = requests.post(url_token, headers=headers_token, data=pay)
+    response_token.raise_for_status()
+    token_info = response_token.json()
+    return token_info['access_token']
 
 if __name__ == "__main__":
     access_token = get_access_token()
